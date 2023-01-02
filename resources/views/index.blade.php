46b3d0cc36e8923706bfdd69ea9375914de1e68f
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-03 00:34:35 +0900

    ページごとに内容を変えよう

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        <h1>My BBS</h1>
+<x-layout>
+    <h1>My BBS</h1>
         <ul>
             @forelse ($posts as $index => $post)
                 <li>
@@ -21,6 +11,4 @@
                 <li>No posts yet!</li>
             @endforelse
         </ul>
-    </div>
-</body>
-</html>
+</x-layout>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,16 +1,9 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My BBS</title>
-    <link rel="stylesheet" href="{{ url('css/style.css') }}">
-</head>
-<body>
-    <div class="container">
-        <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
-        <h1>{{ $post }}</h1>
-    </div>
-</body>
-</html>
+<x-layout>
+
+    <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
+    <h1>{{ $post }}</h1>
+
+</x-layout>
+
