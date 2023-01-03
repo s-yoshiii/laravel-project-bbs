d91847aa8de098d86f983c5999ad4ace29a3f1e4
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-03 23:56:28 +0900

    ページごとにタイトルを変えよう

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My BBS</title>
+    <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 <body>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
 <x-layout>
-
+    <x-slot name="title">{{ $post }} | My BBS</x-slot>
     <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
     <h1>{{ $post }}</h1>
 
