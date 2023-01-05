94ccdbce43ed63ac42c2eecfddb0d1f1f4c4106b
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-05 23:43:24 +0900

    投稿フォームへのリンクを作ろう

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <x-slot name="title">My BBS</x-slot>
-    <h1>My BBS</h1>
+    <h1><span>My BBS</span><a href="{{ route('posts.create') }}">[add]</a></h1>
         <ul>
             @forelse ($posts as $post)
                 <li>
