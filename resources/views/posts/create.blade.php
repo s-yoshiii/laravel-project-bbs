ded089fbce10af8dacadfbc20794fcdf3b806c31
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-06 00:09:10 +0900

    フォームにCSRF対策を施そう

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,6 +2,10 @@
     <x-slot name="title">Add New Post | My BBS</x-slot>
     <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
     <h1>Add New Post</h1>
-
+    <form action="" method="post">
+        @csrf
+        <label>Title <input type="text" name="title"></label>
+        <label>Body <textarea name="body"></textarea></label>
+    </form>
 </x-layout>
 
