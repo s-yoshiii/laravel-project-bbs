0ec1957d12ce0b1142b9ad36ba7e0fa66e62d638
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-07 00:04:57 +0900

    エラーメッセージをカスタマイズしよう

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,13 +5,13 @@
     <form action="{{ route('posts.store') }}" method="post">
         @csrf
         <div class="form-group">
-            <label>Title <input type="text" name="title"></label>
+            <label>Title <input type="text" name="title" value="{{ old('title') }}"></label>
             @error('title')
             <div class="error">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
-            <label>Body <textarea name="body"></textarea></label>
+            <label>Body <textarea name="body">{{ old('body') }}</textarea></label>
             @error('body')
             <div class="error">{{ $message }}</div>
             @enderror
