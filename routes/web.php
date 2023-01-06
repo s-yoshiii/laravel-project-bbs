e2a00736fb5125ebd486bcbb5d5ae58bf8c25eae
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-06 21:50:30 +0900

    投稿を保存する処理のエラーを修正

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/posts/{post}', [PostController::class,'show'])
     ->where('post','[0-9]+');
 Route::get('/posts/create', [PostController::class,'create'])
     ->name('posts.create');
-Route::get('/posts/strore', [PostController::class,'store'])
+Route::post('/posts/store', [PostController::class,'store'])
     ->name('posts.store');
