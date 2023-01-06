cba8165c6b46a49001958dcac764e92b2cb53983
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-07 00:17:10 +0900

    e()を使ってみよう

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">{{ $post->title }} | My BBS</x-slot>
     <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
     <h1>{{ $post->title }}</h1>
-    <p>{!! nl2br($post->body) !!}</p>
+    <p>{!! nl2br(e($post->body)) !!}</p>
 
 </x-layout>
 
