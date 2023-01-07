f5a1217f7bbc39eb182362703aacf6dedc4b19ab
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-07 10:47:37 +0900

    編集フォームのViewを作ろう

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,22 +1,22 @@
 <x-layout>
-    <x-slot name="title">Add New Post | My BBS</x-slot>
-    <div class="back-link">&laquo; <a href="{{ route('posts.index') }}">Back</a></div>
+    <x-slot name="title">Edit Post | My BBS</x-slot>
+    <div class="back-link">&laquo; <a href="{{ route('posts.show', $post) }}">Back</a></div>
     <h1>Add New Post</h1>
-    <form action="{{ route('posts.store') }}" method="post">
+    <form action="" method="post">
         @csrf
         <div class="form-group">
-            <label>Title <input type="text" name="title" value="{{ old('title') }}"></label>
+            <label>Title <input type="text" name="title" value="{{ old('title', $post->title) }}"></label>
             @error('title')
             <div class="error">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
-            <label>Body <textarea name="body">{{ old('body') }}</textarea></label>
+            <label>Body <textarea name="body">{{ old('body', $post->body) }}</textarea></label>
             @error('body')
             <div class="error">{{ $message }}</div>
             @enderror
         </div>
-        <div class="form-button"><button type="submit">Add</button></div>
+        <div class="form-button"><button type="submit">Upddate</button></div>
     </form>
 </x-layout>
 
